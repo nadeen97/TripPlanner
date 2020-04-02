ece55e3fc903951bc99a799746e5dbb48f3baddf
AmnaAhmedOk <amnaokasha4@gmail.com>
2020-04-02 03:26:45 +0200

    dictance

--- app/src/main/java/POJOs/HomeTripAdapter.java
+++ app/src/main/java/POJOs/HomeTripAdapter.java
@@ -86,6 +86,7 @@ public class HomeTripAdapter extends RecyclerView.Adapter<HomeTripAdapter.ViewHo
         holder.time_tv.setText(currentItem.getStartTime());
         holder.date_tv.setText(currentItem.getStartDate());
       holder.description_tv.setText(currentItem.getDescription());
+      holder.disctance.setText(currentItem.getDistance());
         boolean isExpandable = currentItem.isExpandable;
         holder.expnadable.setVisibility(isExpandable? View.VISIBLE:View.GONE);
 
@@ -174,7 +175,7 @@ public class HomeTripAdapter extends RecyclerView.Adapter<HomeTripAdapter.ViewHo
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
-        public TextView tripName_tv, destination_tv,source_tv,time_tv,date_tv ,buttonViewOption,description_tv;
+        public TextView tripName_tv, destination_tv,source_tv,time_tv,date_tv ,buttonViewOption,description_tv, disctance;
         LinearLayout continar;
          View expnadable;
          Button start ,showNotes;
@@ -192,6 +193,7 @@ public class HomeTripAdapter extends RecyclerView.Adapter<HomeTripAdapter.ViewHo
             continar = itemView.findViewById(R.id.container_layout);
             start = itemView.findViewById(R.id.start_btn);
             showNotes = itemView.findViewById(R.id.notes_btn);
+            disctance = itemView.findViewById(R.id.distanceTV);
 
         }
     }
